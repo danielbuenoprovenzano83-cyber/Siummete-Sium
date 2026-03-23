1fed9366de1578f5eea636ba39858d1acfb5cd55
danielbuenoprovenzano83-cyber <danielbuenoprovenzano83@gmail.com>
2026-03-23 10:26:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const { Boom } = require("@hapi/boom");
 const qrcode = require("qrcode-terminal");
 const express = require('express');
 const mongoose = require("mongoose");
-const { useMongoDBAuthState } = require("baileys-mongodb-storage");
+const { useMongoDBAuthState } = require("@whiskeysockets/baileys-mongodb-storage");
 
 // --- SERVER PER RENDER ---
 const app = express();
